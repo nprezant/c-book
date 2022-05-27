a36e3c689ebd8b81af5d4afa7f18b70b820dab53
boom-roasted <noah.prezant@protonmail.ch>
2022-05-27 00:48:52 -0600

    Use symbolic constants and a for loop

--- ch01/fahr.c
+++ ch01/fahr.c
@@ -3,21 +3,15 @@
 /* print Fahrenheit-Celsius table
  * for fahr = 0, 20, ..., 300 */
 
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+
 main()
 {
-    float fahr, celsius;
-    int lower, upper, step;
-    
-    lower = 0;
-    upper = 300;
-    step = 20;
-
-    fahr = lower;
+    int fahr;
 
     printf("%s\t%s\n", "Fahr", "Celsius");
-    while (fahr <= upper) {
-        celsius = 5 * (fahr-32) / 9;
-        printf("%3.0f\t%6.1f\n", fahr, celsius);
-        fahr = fahr + step;
-    }
+    for (fahr = LOWER; fahr <= UPPER; fahr += STEP)
+        printf("%3d\t%6.1f\n", fahr, 5.0 / 9.0 * (fahr - 32));
 }
