806e15cb441f9e3c5356ab0ce311c55710d0e8c6
boom-roasted <noah.prezant@protonmail.ch>
2022-07-16 23:15:02 -0600

    Add program to print one word per line

---
+++ ch01/1-12_split_words.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+#define IN 1
+#define OUT 2
+
+int main() {
+    int c, state;
+
+    state = IN;
+
+    while ((c = getchar()) != EOF) {
+        if (c == ' ' || c == '\t' || c == '\n')
+            state = OUT;
+        else if (state == OUT) {
+            state = IN;
+            putchar('\n');
+        }
+        if (state == IN)
+            putchar(c);
+    }
+}
