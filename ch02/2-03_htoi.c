b3c66427da4e5858b99735cf7c83781990a51ac0
boom-roasted <noah.prezant@protonmail.ch>
2022-09-23 14:57:24 -0600

    Add hexadecimal to decimal converter

---
+++ ch02/2-03_htoi.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <ctype.h>
+
+#define BUFFSIZE 1000
+
+/* Converts hexadecimal string to an integer */
+int htoi(char s[]);
+
+int trimtrailing(char s[], int len);
+int getline_(char s[], int lim);
+
+int main() {
+
+    int len, i;
+    char buff[BUFFSIZE];
+
+    while ((len = getline_(buff, BUFFSIZE)) > 0) {
+        trimtrailing(buff, len);
+        printf(
+                "Converted \"%s\" in hexadecimal to %d\n",
+                buff,
+                htoi(buff));
+    }
+
+}
+
+int htoi(char s[]) {
+    int i, n;
+
+    n = 0;
+    for (i=0; isdigit(s[i]) || 'a' <= tolower(s[i]) && tolower(s[i]) <= 'f'; ++i) {
+        n *= 16;
+        if (isdigit(s[i]))
+            n += s[i] - '0';
+        else
+            n += 10 + tolower(s[i]) - 'a';
+    }
+    return n;
+}
+
+int trimtrailing(char s[], int len) {
+    while(len>0 && (s[len-1]==' ' || s[len-1]=='\t' || s[len-1]=='\n')) {
+        --len;
+    }
+    s[len] = '\0';
+    return len;
+}
+
+int getline_(char s[], int lim) {
+    int c, i;
+    for (i=0; i < lim-1 && (c=getchar())!=EOF && c!='\n'; ++i)
+        s[i] = c;
+    if (c == '\n') {
+        s[i] = c;
+        ++i;
+    }
+    s[i] = '\0';
+    return i;
+}
